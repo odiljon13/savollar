47dccf680197f2e351e464d10d2fd804a7ad740e
faroxiddin02 <faroxiddin02@gmail.com>
2026-08-06 14:20:30 +0500

    ss

--- backend/app/config.py
+++ backend/app/config.py
@@ -13,6 +13,6 @@ for env_path in (
         break
 
 ADMIN_USERNAME = os.getenv('ADMIN_USERNAME', 'admin123')
-ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD', '09876543211234567890A')
+ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD', '1234567890')
 DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql+psycopg2://postgres:postgres@localhost:5432/logicquest')
 API_PREFIX = '/api'
